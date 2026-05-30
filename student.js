55f9ef33afe0ca1dd3f628a800f9e3ac07845bee
Marzouk DAOUDA <marzoukdaouda23@gmail.com>
2026-05-30 23:03:10 +0100

    Amélioration du design du formulaire

--- student.js
+++ student.js
@@ -1,1 +1,5 @@
 console.log("Application de gestion des étudiants initialisée.");
+document.getElementById('student-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.log("Un étudiant a été ajouté !");
+});
